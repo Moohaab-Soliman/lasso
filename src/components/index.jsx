0bcc822e7f0ea24b0793fe54ea24a3d7cf063ed0
Moohab-Soliman <mohab.soliman@tu-clausthal.de>
2020-10-18 08:44:04 +0200

    first commit

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -16,81 +16,81 @@ class Index extends Component {
     isLoading: true,
   };
 
-  handleConsole = () => {
-    const db = firebase.firestore();
+  // handleConsole = () => {
+  //   const db = firebase.firestore();
 
-    const {
-      match: { params },
-    } = this.props;
+  //   const {
+  //     match: { params },
+  //   } = this.props;
 
-    // if (params.id != null) {
-    //   db.collection("profile")
-    //     .where("username", "==", params.userId)
-    //     .get()
-    //     .then((value) => {
-    //       db.collection("profile")
-    //         .doc(value.docs[0].id)
-    //         .update({
-    //           nOfTabs: firebase.firestore.FieldValue.increment(1),
-    //         });
-    //     });
-    // }
+  //   // if (params.id != null) {
+  //   //   db.collection("profile")
+  //   //     .where("username", "==", params.userId)
+  //   //     .get()
+  //   //     .then((value) => {
+  //   //       db.collection("profile")
+  //   //         .doc(value.docs[0].id)
+  //   //         .update({
+  //   //           nOfTabs: firebase.firestore.FieldValue.increment(1),
+  //   //         });
+  //   //     });
+  //   // }
 
-    axios
-      .get(`${params.userId}`)
-      .then(
-        db
-          .collection("profile")
-          .where("username", "==", params.userId)
-          .onSnapshot((querySnapshot) => {
-            console.log(querySnapshot);
-            this.setState({ isLoading: false });
-            let userData = [];
-            querySnapshot.docs.length > 0
-              ? this.setState({ isUserFound: true })
-              : this.setState({ isUserFound: false });
+  //   axios
+  //     .get(`${params.userId}`)
+  //     .then(
+  //       db
+  //         .collection("profile")
+  //         .where("username", "==", params.userId)
+  //         .onSnapshot((querySnapshot) => {
+  //           console.log(querySnapshot);
+  //           this.setState({ isLoading: false });
+  //           let userData = [];
+  //           querySnapshot.docs.length > 0
+  //             ? this.setState({ isUserFound: true })
+  //             : this.setState({ isUserFound: false });
 
-            querySnapshot.forEach((doc) => {
-              userData.push(doc.data());
-              this.setState({ userData });
-            });
-            //     if (userData.length >= 1) {
-            //       db.collection("profile")
-            //         .doc(querySnapshot.docs[0].id)
-            //         .collection("userSocialLinks")
-            //         .get()
-            //         .then((mydata) => {
-            //           let getSocialLinks = [];
+  //           querySnapshot.forEach((doc) => {
+  //             userData.push(doc.data());
+  //             this.setState({ userData });
+  //           });
+  //           //     if (userData.length >= 1) {
+  //           //       db.collection("profile")
+  //           //         .doc(querySnapshot.docs[0].id)
+  //           //         .collection("userSocialLinks")
+  //           //         .get()
+  //           //         .then((mydata) => {
+  //           //           let getSocialLinks = [];
 
-            //           mydata.forEach((doc) => {
-            //             getSocialLinks.push(doc.data());
-            //             this.setState({
-            //               getSocialLinks,
-            //             });
-            //           });
-            //         });
-            //     } else {
-            //     }
-          })
-      )
-      .catch(function (error) {
-        // handle error
-        console.log(error);
-      })
-      .then(function () {
-        // always executed
-      });
-  };
+  //           //           mydata.forEach((doc) => {
+  //           //             getSocialLinks.push(doc.data());
+  //           //             this.setState({
+  //           //               getSocialLinks,
+  //           //             });
+  //           //           });
+  //           //         });
+  //           //     } else {
+  //           //     }
+  //         })
+  //     )
+  //     .catch(function (error) {
+  //       // handle error
+  //       console.log(error);
+  //     })
+  //     .then(function () {
+  //       // always executed
+  //     });
+  // };
 
-  componentDidMount() {
-    this.handleConsole();
-  }
+  // componentDidMount() {
+  //   this.handleConsole();
+  // }
 
   render() {
     const { isUserFound, isLoading, userData, getSocialLinks } = this.state;
 
-    if (isLoading) return <h1 className="loader">.</h1>;
-    if (!isUserFound) return <p>User not found</p>;
+    // if (isLoading) return <h1 className="loader">.</h1>;
+    // if (!isUserFound) return <p>User not found</p>;
 
     return (
       <div className="App">
