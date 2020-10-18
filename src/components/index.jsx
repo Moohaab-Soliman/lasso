72b18181dfe9c33e14484c7a15581babecc9f811
Moohab-Soliman <mohab.soliman@tu-clausthal.de>
2020-10-18 08:40:55 +0200

    first commit

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -39,40 +39,39 @@ class Index extends Component {
     axios
       .get(`${params.userId}`)
       .then(
-        console.log(params.userId, params.id)
-        // db
-        //   .collection("profile")
-        //   .where("username", "==", params.userId)
-        //   .onSnapshot((querySnapshot) => {
-        //     console.log(querySnapshot);
-        //     this.setState({ isLoading: false });
-        //     let userData = [];
-        //     querySnapshot.docs.length > 0
-        //       ? this.setState({ isUserFound: true })
-        //       : this.setState({ isUserFound: false });
+        db
+          .collection("profile")
+          .where("username", "==", params.userId)
+          .onSnapshot((querySnapshot) => {
+            console.log(querySnapshot);
+            this.setState({ isLoading: false });
+            let userData = [];
+            querySnapshot.docs.length > 0
+              ? this.setState({ isUserFound: true })
+              : this.setState({ isUserFound: false });
 
-        //     querySnapshot.forEach((doc) => {
-        //       userData.push(doc.data());
-        //       this.setState({ userData });
-        //     });
-        //     if (userData.length >= 1) {
-        //       db.collection("profile")
-        //         .doc(querySnapshot.docs[0].id)
-        //         .collection("userSocialLinks")
-        //         .get()
-        //         .then((mydata) => {
-        //           let getSocialLinks = [];
+            querySnapshot.forEach((doc) => {
+              userData.push(doc.data());
+              this.setState({ userData });
+            });
+            //     if (userData.length >= 1) {
+            //       db.collection("profile")
+            //         .doc(querySnapshot.docs[0].id)
+            //         .collection("userSocialLinks")
+            //         .get()
+            //         .then((mydata) => {
+            //           let getSocialLinks = [];
 
-        //           mydata.forEach((doc) => {
-        //             getSocialLinks.push(doc.data());
-        //             this.setState({
-        //               getSocialLinks,
-        //             });
-        //           });
-        //         });
-        //     } else {
-        //     }
-        //   })
+            //           mydata.forEach((doc) => {
+            //             getSocialLinks.push(doc.data());
+            //             this.setState({
+            //               getSocialLinks,
+            //             });
+            //           });
+            //         });
+            //     } else {
+            //     }
+          })
       )
       .catch(function (error) {
         // handle error
