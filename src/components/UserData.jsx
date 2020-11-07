a58a291f124d909e01c9f80e80124f78ad081029
Moohab-Soliman <mohab.soliman@tu-clausthal.de>
2020-11-07 02:20:53 +0100

    your message

--- src/components/UserData.jsx
+++ src/components/UserData.jsx
@@ -46,15 +46,28 @@ const useStyles = makeStyles((theme) => ({
     margin: theme.spacing(0.5),
     display: "inline-block",
   },
+  verifiedIcon: {
+    width: "15px",
+    height: "15px",
+    marginLeft: "5px",
+    marginTop: "5px",
+  },
 }));
 
 const UserData = (props) => {
   const { message } = props;
+  const checkImg =
+    "https://firebasestorage.googleapis.com/v0/b/lasso-fc13c.appspot.com/o/images%2Fcheck.png?alt=media&token=2694b679-6c51-4a69-9382-8f99465c6044";
   const classes = useStyles();
   return (
     <div className={classes.root}>
       <div>
-        <p className={classes.name}>{message.displayName}</p>
+        <p className={classes.name}>
+          {message.displayName}
+          {message.isVerified === true ? (
+            <img src={checkImg} className={classes.verifiedIcon} />
+          ) : null}
+        </p>
 
         <Typography variant="subtitle2" className={classes.id}>
           @{message.username}
